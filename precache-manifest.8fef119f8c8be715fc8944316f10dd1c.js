752092e27ebb8edbcb73f9a0752c7599c2c41785
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-10-27 22:33:03 +0700

    portfolio 1.0

---
+++ precache-manifest.8fef119f8c8be715fc8944316f10dd1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4553b0caef5e7deb0fba11d0641e096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dea79b6927638af5cc14",
+    "url": "/static/css/main.98057ec1.chunk.css"
+  },
+  {
+    "revision": "381aeeb63c47708720e5",
+    "url": "/static/js/2.ff5260f9.chunk.js"
+  },
+  {
+    "revision": "dea79b6927638af5cc14",
+    "url": "/static/js/main.c510f9de.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
