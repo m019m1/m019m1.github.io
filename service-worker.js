543b317c59b969e8883e64f2d85e5551e2ab21ca
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-11-15 00:41:20 +0700

    fixed project' description

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2002d6831850cef441605116d4cbbded.js"
+  "/precache-manifest.32279a1679f1c384c26bcc0fcb4a8e7f.js"
 );
 
 self.addEventListener('message', (event) => {
