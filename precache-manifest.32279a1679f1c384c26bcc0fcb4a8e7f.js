2197af622ad3acd2327ad709e252b25b7acf0c0b
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-11-15 00:47:52 +0700

    fixed-2 project' description

---
+++ precache-manifest.32279a1679f1c384c26bcc0fcb4a8e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919e54d17ef460d02027f122e70f19c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1338b837e3e2e0d4d8d",
+    "url": "/static/css/main.6c40fe69.chunk.css"
+  },
+  {
+    "revision": "09af34fca3b9aada436d",
+    "url": "/static/js/2.9042f9fd.chunk.js"
+  },
+  {
+    "revision": "d1338b837e3e2e0d4d8d",
+    "url": "/static/js/main.80a7d942.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "2bb726092475d0c9918ce6a19ee9dffa",
+    "url": "/static/media/background.2bb72609.jpg"
+  },
+  {
+    "revision": "0844b0fcdf3addde9baaab321e3c7a49",
+    "url": "/static/media/me.0844b0fc.jpg"
+  }
+]);
